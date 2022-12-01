e7238b81ff1a6e055289d058b08ba05e5424727a
Nartyna <mplutwska@gmail.com>
2022-12-01 20:15:11 +0100

    [01.12] Second Star

--- AOC_1/AOC_1/AOC_1.cpp
+++ AOC_1/AOC_1/AOC_1.cpp
@@ -7,6 +7,8 @@ int main()
     std::ifstream file("input.txt");
     std::string line;
     int max = 0;
+    int max2 = 0;
+    int max3 = 0;
     int calories = 0;
     while (std::getline(file, line))
     {
@@ -16,12 +18,25 @@ int main()
         }
         else
         {
-            if (calories > max)
-            {
+            if (calories > max) {
+                max3 = max2;
+                max2 = max;
                 max = calories;
+            }
+            else if (calories > max2) {
+                max3 = max2;
+                max2 = calories;
+            }
+            else if (calories > max3) {
+                max3 = calories;
             }
             calories = 0;
         }
     }
-    std::cout << max;
+            std::cout << max << std::endl;
+            std::cout << max2 << std::endl;
+            std::cout << max3 << std::endl;
+            std::cout << max+max2+max3 << std::endl;
+            std::cout << std::endl;
+            std::cout << std::endl;
 }
