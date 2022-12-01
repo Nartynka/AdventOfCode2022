0c4196aa0f1ea53b29be6fd8840ec176953e00f8
Nartyna <mplutwska@gmail.com>
2022-12-01 19:08:25 +0100

    [01.12] First Star

---
+++ AOC_1/AOC_1/AOC_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main()
+{
+    std::ifstream file("input.txt");
+    std::string line;
+    int max = 0;
+    int calories = 0;
+    while (std::getline(file, line))
+    {
+        if (line != "")
+        {
+            calories += std::stoi(line);
+        }
+        else
+        {
+            if (calories > max)
+            {
+                max = calories;
+            }
+            calories = 0;
+        }
+    }
+    std::cout << max;
+}
