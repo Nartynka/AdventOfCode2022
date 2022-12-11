2a4094c3af6bd65c3302d4d9af818bca9f01c2e1
Nartyna <mplutwska@gmail.com>
2022-12-11 11:22:15 +0100

    [10.12] Second Star

--- AOC_10/AOC_10/AOC_10.cpp
+++ AOC_10/AOC_10/AOC_10.cpp
@@ -5,6 +5,8 @@
 // addx V - 2 cycles
 // noop - 1 cycle
 
+// 40 wide 6 high
+
 int main()
 {
     std::ifstream file("input.txt");
@@ -12,20 +14,29 @@ int main()
 
     int x = 1;
     int cycle = 1;
-    int sum = 0;
+    int crt = 0;
     while (std::getline(file, line))
     {
+        int spos = x;
         int to_do = 2;
         if (line == "noop")
             to_do = 1;
+
         for (; to_do > 0; to_do--)
         {
-            if (cycle == 20 || cycle == 60 || cycle == 100 || cycle == 140 || cycle == 180 || cycle == 220)
-                sum += cycle * x;
+            if (spos == crt || spos+1==crt || spos-1==crt)
+                std::cout << "#";
+            else
+                std::cout << ".";
+            if (crt == 39) {
+                std::cout<<std::endl;
+            }
+            crt++;
+            if (crt > 39)
+                crt = 0;
             cycle++;
         }
         if (line != "noop")
             x += std::stoi(line.substr(5));
     }
-    std::cout << "Sum: " << sum;
 }
